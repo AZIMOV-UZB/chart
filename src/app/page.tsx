daf3d14167aa3cc581126c89e86dd4266353832a
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2025-01-22 10:19:48 +0500

    azimov

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Component } from "@/components/area-chart";
+import { Chart } from "@/components/chart";
+import { Piechart } from "@/components/pie-chart";
+
+export default function Home() {
+  return (
+    <div className="container max-w-[1250px] mx-auto">
+      <Component />
+      <Chart />
+      <Piechart />
+    </div>
+  );
+}
